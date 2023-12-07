4d011ce70c2269ff187d608e41847e2d575c9af8
vicente <vicentecostaalmeida@gmail.com>
2023-12-07 16:35:37 +0100

    setup automerge repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import { Editor } from "./Editor.tsx";
+import { Repo } from "@automerge/automerge-repo";
+import { BroadcastChannelNetworkAdapter } from "@automerge/automerge-repo-network-broadcastchannel";
+import { IndexedDBStorageAdapter } from "@automerge/automerge-repo-storage-indexeddb";
+import { useState } from "react";
+
+export interface DocType {
+  content: string;
+}
+
+function initRepo() {
+  return new Repo({
+    network: [new BroadcastChannelNetworkAdapter()],
+    storage: new IndexedDBStorageAdapter(),
+  });
+}
 
 function App() {
-  return <Editor />;
+  const [repo] = useState(initRepo);
+  const [docHandle] = useState(() => repo.create<DocType>());
+
+  return <Editor docHandle={docHandle} />;
 }
 
 export default App;
--- src/Editor.tsx
+++ src/Editor.tsx
@@ -5,22 +5,35 @@ import { EditorView } from "prosemirror-view";
 import { exampleSetup } from "prosemirror-example-setup";
 import "prosemirror-example-setup/style/style.css";
 import "prosemirror-menu/style/menu.css";
+import { DocHandle } from "@automerge/automerge-repo";
+import { DocType } from "./App.tsx";
 
-export function Editor() {
-  const editorRoot = useRef(null);
-  const editorRef = useRef<EditorView | null>(null);
+interface EditorProps {
+  docHandle: DocHandle<DocType>;
+}
+
+export function Editor({ docHandle }: EditorProps) {
+  const mountTargetRef = useRef(null);
 
   useEffect(() => {
-    if (!editorRef.current) {
-      editorRef.current = new EditorView(editorRoot.current, {
-        state: EditorState.create({
-          schema,
-          plugins: exampleSetup({ schema }),
-        }),
-      });
+    const doc = docHandle.docSync();
+    // TODO: handle loading, error states
+    if (!doc) {
+      throw Error("doc is not ready");
     }
-  }, []);
+
+    const view = new EditorView(mountTargetRef.current, {
+      state: EditorState.create({
+        schema,
+        plugins: exampleSetup({ schema }),
+      }),
+    });
+
+    return () => {
+      view.destroy();
+    };
+  }, [docHandle]);
 
   // "editor" id is required so that styles are applied properly
-  return <div ref={editorRoot} id={"editor"} />;
+  return <div ref={mountTargetRef} id={"editor"} />;
 }
