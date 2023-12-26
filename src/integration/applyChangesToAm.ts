4f09990d2b390642298158c9e378fd5ef6d7ed68
vicente <vicentecostaalmeida@gmail.com>
2023-12-26 12:58:38 +0000

    add support for copy and paste of inline text with marks

--- src/integration/applyChangesToAm.ts
+++ src/integration/applyChangesToAm.ts
@@ -1,6 +1,6 @@
 import { DocHandle } from "@automerge/automerge-repo";
 import { DocType } from "../App.tsx";
-import { next as Automerge } from "@automerge/automerge";
+import { next as Automerge } from "automerge-blocks3";
 import { Transaction } from "prosemirror-state";
 import {
   AddMarkStep,
@@ -11,12 +11,16 @@ import {
 function getMarkExpandProperty(mark: string) {
   switch (mark) {
     case "strong":
-    case "italic":
+    case "em":
       return "after";
     // TODO: add explicit statements for comment, link marks
     default:
       return "none";
   }
+}
+
+function mapPmIndexToAm(index: number) {
+  return index - 1;
 }
 
 export function applyChangesToAm(
@@ -29,43 +33,48 @@ export function applyChangesToAm(
   let newHeads;
 
   transaction.steps.forEach((step) => {
+    // this path handles pasting and inserting text
+    // for each text node:
+    // - we insert the corresponding text
+    // - reconcile the marks in the doc so that they are consistent with the marks contained in the text node
     if (step instanceof ReplaceStep) {
-      // at the moment we are only handling inline text nodes
-      // in the future we will support blocks
-      if (
-        // deletion
-        step.slice.content.size !== 0 &&
-        // insertion
-        (step.slice.content.childCount > 1 ||
-          !step.slice.content.firstChild?.isText)
-      ) {
-        throw new Error("not yet supported");
-      }
-
-      const textNode = step.slice.content.firstChild;
-      const textToInsert = textNode?.text;
-      const textMarks = textNode?.marks;
-
       newHeads = docHandle.changeAt(lastHeads, (doc) => {
-        Automerge.splice(
-          doc,
-          // splice() mutates path argument
-          PATH.slice(),
-          step.from - 1,
-          step.to - step.from,
-          textToInsert,
-        );
+        let currAmIndex = mapPmIndexToAm(step.from);
 
-        // step inserts characters
-        if (step.slice.content.childCount > 0) {
+        // first remove any deleted characters
+        // is the case when user presses backspace or selection is not empty and user deletes or inserts characters
+        if (step.to - step.from > 0) {
+          Automerge.splice(doc, PATH.slice(), currAmIndex, step.to - step.from);
+        }
+
+        step.slice.content.forEach((node) => {
+          if (!node.isText) {
+            throw Error(
+              "Error while handling ReplaceStep: only text nodes are supported at the moment",
+            );
+          }
+
+          if (!node.text || node.text.length === 0) {
+            return;
+          }
+
+          Automerge.splice(
+            doc,
+            // splice() mutates path argument
+            PATH.slice(),
+            currAmIndex,
+            0,
+            node.text,
+          );
+
           // reconcile the marks in the doc with the marks of the inserted text
           // this is necessary, when:
           // - selection is empty (otherwise we apply the mark in the AddMarkStep)
           // - mark is not implicit, e.g. inserted character should be bolded, but previous characters aren't
-          const amMarks = Automerge.marksAt(doc, PATH.slice(), step.from - 1);
+          const amMarks = Automerge.marksAt(doc, PATH.slice(), currAmIndex);
 
           // add missing marks
-          for (const textMark of textMarks ?? []) {
+          for (const textMark of node.marks ?? []) {
             const isMarkInDoc =
               textMark.type.name in amMarks && !!amMarks[textMark.type.name];
 
@@ -74,8 +83,8 @@ export function applyChangesToAm(
                 doc,
                 PATH.slice(),
                 {
-                  start: step.from - 1,
-                  end: step.to,
+                  start: currAmIndex,
+                  end: currAmIndex + node.text.length,
                   expand: "after",
                 },
                 textMark.type.name,
@@ -91,7 +100,7 @@ export function applyChangesToAm(
               continue;
             }
 
-            const hasInsertedTextMark = textMarks
+            const hasInsertedTextMark = node.marks
               ?.map((m) => m.type.name)
               ?.includes(amMark);
 
@@ -100,15 +109,17 @@ export function applyChangesToAm(
                 doc,
                 PATH.slice(),
                 {
-                  start: step.from - 1,
-                  end: step.to,
+                  start: currAmIndex,
+                  end: currAmIndex + node.text.length,
                   expand: "after",
                 },
                 amMark,
               );
             }
           }
-        }
+
+          currAmIndex += node.text.length;
+        });
       });
     }
 
