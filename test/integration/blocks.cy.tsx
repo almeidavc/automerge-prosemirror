06a1df2253632bc2cc585080e485ad758d6a8e65
vicente <vicentecostaalmeida@gmail.com>
2024-01-03 12:01:34 +0100

    write tests for block operations when selection is present

--- test/integration/blocks.cy.tsx
+++ test/integration/blocks.cy.tsx
@@ -74,4 +74,91 @@ describe("blocks", () => {
         expect(actualDoc).to.deep.equal(expectedDoc);
       });
   });
+
+  it("selecting characters and pressing enter, deletes the characters and creates a new block", () => {
+    cy.get(PM_EDITOR).type("fox");
+    cy.get(PM_EDITOR).type("{selectAll}");
+    cy.get(PM_EDITOR)
+      .type("{enter}")
+      .then(() => {
+        const spans = Automerge.spans(docHandle.docSync()!, ["text"]);
+        expect(spans).to.deep.equal([
+          { type: "block", value: { type: "paragraph" } },
+        ]);
+
+        const actualDoc = editorViewRef.current?.state.doc;
+        const { doc: expectedDoc } = EditorState.create({
+          doc: EditorSchema.node(EditorSchema.topNodeType, null, [
+            EditorSchema.node("paragraph", null),
+            EditorSchema.node("paragraph", null),
+          ]),
+        });
+        expect(actualDoc).to.deep.equal(expectedDoc);
+      });
+  });
+
+  it(
+    "selecting range of characters that includes block delimiters and pressing backspace, " +
+      "deletes selected characters and joins blocks",
+    () => {
+      cy.get(PM_EDITOR).type("fox{enter}fox");
+      cy.get(PM_EDITOR).realPress([
+        "ArrowLeft",
+        "ArrowLeft",
+        "Shift",
+        "ArrowLeft",
+        "ArrowLeft",
+        "ArrowLeft",
+      ]);
+      cy.get(PM_EDITOR)
+        .type("{backspace}")
+        .then(() => {
+          const spans = Automerge.spans(docHandle.docSync()!, ["text"]);
+          expect(spans).to.deep.equal([{ type: "text", value: "foox" }]);
+
+          const actualDoc = editorViewRef.current?.state.doc;
+          const { doc: expectedDoc } = EditorState.create({
+            doc: EditorSchema.node(EditorSchema.topNodeType, null, [
+              EditorSchema.node("paragraph", null, EditorSchema.text("foox")),
+            ]),
+          });
+          expect(actualDoc).to.deep.equal(expectedDoc);
+        });
+    },
+  );
+
+  it(
+    "selecting range of characters that includes block delimiters and pressing enter, " +
+      "deletes selected characters and block structure doesn't change",
+    () => {
+      cy.get(PM_EDITOR).type("fox{enter}fox");
+      cy.get(PM_EDITOR).realPress([
+        "ArrowLeft",
+        "ArrowLeft",
+        "Shift",
+        "ArrowLeft",
+        "ArrowLeft",
+        "ArrowLeft",
+      ]);
+      cy.get(PM_EDITOR)
+        .type("{enter}")
+        .then(() => {
+          const spans = Automerge.spans(docHandle.docSync()!, ["text"]);
+          expect(spans).to.deep.equal([
+            { type: "text", value: "fo" },
+            { type: "block", value: { type: "paragraph" } },
+            { type: "text", value: "ox" },
+          ]);
+
+          const actualDoc = editorViewRef.current?.state.doc;
+          const { doc: expectedDoc } = EditorState.create({
+            doc: EditorSchema.node(EditorSchema.topNodeType, null, [
+              EditorSchema.node("paragraph", null, EditorSchema.text("fo")),
+              EditorSchema.node("paragraph", null, EditorSchema.text("ox")),
+            ]),
+          });
+          expect(actualDoc).to.deep.equal(expectedDoc);
+        });
+    },
+  );
 });
